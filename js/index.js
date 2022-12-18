74108a670fd4fadb83ead688c161bf253d69acd7
天翔 <11820134+ilovechenyu@user.noreply.gitee.com>
2022-12-18 10:47:53 +0000

    4544
    Signed-off-by: 天翔 <11820134+ilovechenyu@user.noreply.gitee.com>

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//
-    if(userName=="Song Yinuo" &&  pwd=="happy"){
+    if(userName=="Song Yinuo" &&  pwd=="20021218"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
